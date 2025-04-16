8713391d98627434d9ceef904638d724d45562b1
enjoyer-of-coffee <askhat.zilmukhamedov@outlook.com>
2025-04-15 20:48:24 -0600

    updated App

--- src/main/java/com/mezozoo/App.java
+++ src/main/java/com/mezozoo/App.java
@@ -2,6 +2,12 @@ package com.mezozoo;
 
 public class App {
     public static void main(String[] args) {
-
+        System.out.println("Welcome to Mesozoic Eden!");
+        System.out.println();
+        String openHours = "08:00";
+        String closingHours = "20:00";
+        System.out.println("Hours of work: " + openHours
+        + " - " + closingHours + " everyday.");
+        System.out.println("Mesozoic Eden is safe and secure place.");
     }
 }
